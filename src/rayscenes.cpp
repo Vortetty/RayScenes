e2603918c05131f382f655de9cd30c88827594dd
Winter <vortetty@gmail.com>
2021-10-10 20:36:23 -0700

    I apparently forgot how to properly write code last time i tried to do things so i fixed it

--- src/rayscenes.cpp
+++ src/rayscenes.cpp
@@ -1,4 +1,4 @@
-#pragma once
+#include "rayscenes.hpp"
 
 #include <deque>
 #include <unordered_map>
@@ -8,25 +8,31 @@
 #include <tuple>
 
 namespace rayscenes {
-	class rayscenemanager {
-	public:
-		typedef void(*rayscenefunc)(rayscenes::rayscenemanager&, bool);
-		
-        class rayscene {
-        public:
-            rayscene(rayscenefunc _func) { func = _func; funcSet = true; }
-            rayscene() {};
+	// 
+	// Class | rayscenemanager::rayscene 
+	//
+		//
+		// Initializers
+		//
+		rayscenemanager::rayscene::rayscene(rayscenefunc _func) { func = _func; funcSet = true; }
+		rayscenemanager::rayscene::rayscene() {};
 
-            void setFunc(rayscenefunc _func) { func = _func; funcSet = true; }
-            void call(rayscenemanager &rsm, bool isTop) { if (funcSet) func(rsm, isTop); }
+		//
+		// Utilities
+		//
+		void rayscenemanager::rayscene::setFunc(rayscenefunc _func) { func = _func; funcSet = true; }
+		void rayscenemanager::rayscene::call(rayscenemanager &rsm, bool isTop) { if (funcSet) func(rsm, isTop); }
 
-        private:
-            bool funcSet = true;
-            rayscenefunc func;
-        };
 
-		rayscenemanager() {}
-		rayscenemanager(const rayscenemanager& r) {
+	//
+	// Class | rayscenemanager
+	//
+
+		//
+		// Initializers
+		//
+		rayscenemanager::rayscenemanager() {}
+		rayscenemanager::rayscenemanager(const rayscenemanager& r) {
 			clear_unordered_map(scenes);
 			clear_deque(activeScenes);
 
@@ -34,59 +40,80 @@ namespace rayscenes {
 			for (auto &scene : ((rayscenemanager)r).getActiveScenes()) activeScenes.push_back(scene);
 		}
 
-		std::unordered_map<std::string, rayscene>& getAllScenes() { return scenes; }
-		void setAllScenes(std::unordered_map<std::string, rayscene> newScenes) { for (auto &[key, scene] : newScenes) scenes[key] = scene; }
-		rayscene& getScene(std::string id) { return scenes[id]; }
-		void addScene(std::string id, rayscene scene) { scenes[id] = scene; }
-		void addScene(std::string id, rayscenefunc func) { scenes[id] = rayscene(func); }
-		void removeScene(std::string id) { scenes.erase(id); }
+		//
+		// Basic scene management
+		//
+		std::unordered_map<std::string, rayscenemanager::rayscene>& rayscenemanager::getAllScenes() { 
+			return scenes; 
+		}
+		void                                                        rayscenemanager::setAllScenes(std::unordered_map<std::string, rayscene> newScenes) { 
+			for (auto &[key, scene] : newScenes) scenes[key] = scene; 
+		}
+		rayscenemanager::rayscene&                                  rayscenemanager::getScene(std::string id) { 
+			return scenes[id]; 
+		}
+		void                                                        rayscenemanager::addScene(std::string id, rayscene scene) { 
+			scenes[id] = scene; 
+		}
+		void                                                        rayscenemanager::addScene(std::string id, rayscenefunc func) { 
+			scenes[id] = rayscene(func);
+		}
+		void                                                        rayscenemanager::removeScene(std::string id) { 
+			scenes.erase(id); 
+		}
 
-		std::deque<std::string>& getActiveScenes() { return activeScenes; }
-		void setActiveScenes(std::deque<std::string> ids) {
+		//
+		// Active scene management
+		//
+		std::deque<std::string>& rayscenemanager::getActiveScenes() { return activeScenes; }
+		void                     rayscenemanager::setActiveScenes(std::deque<std::string> ids) {
 			clear_deque(activeScenes);
+			for (auto &id : ids)
+				activeScenes.push_back(id);
+		}
+		void                     rayscenemanager::setActiveScene(std::string id) {
+			clear_deque(activeScenes);
+			activeScenes.push_back(id);
+		}
+		void                     rayscenemanager::addActiveScene(std::string id) { 
+			activeScenes.push_back(id); 
+		}
+		void                     rayscenemanager::addActiveScenes(std::deque<std::string> ids) { 
 			for (auto &id : ids) 
-                activeScenes.push_back(id);
+				activeScenes.push_back(id); 
 		}
-		void setActiveScene(std::string id) { 
-			clear_deque(activeScenes);
-            activeScenes.push_back(id); 
-        }
-		void addActiveScene(std::string id) { activeScenes.push_back(id); }
-		void addActiveScenes(std::deque<std::string> ids) { for (auto &id : ids) activeScenes.push_back(id); }
+		void                     rayscenemanager::removeActiveScene(std::string id) { 
+			activeScenes.erase(std::remove(activeScenes.begin(), activeScenes.end(), id), activeScenes.end()); 
+		}
+		void                     rayscenemanager::removeActiveScenes(std::deque<std::string> ids) {
+			for (auto &id : ids)
+				activeScenes.erase(std::remove(activeScenes.begin(), activeScenes.end(), id), activeScenes.end());
+		}
+		void                     rayscenemanager::clearActiveScenes(std::string id) { 
+			activeScenes.erase(std::remove(activeScenes.begin(), activeScenes.end(), id), activeScenes.end()); 
+		}
 
-		void removeActiveScene(std::string id) { activeScenes.erase(std::remove(activeScenes.begin(), activeScenes.end(), id), activeScenes.end()); }
-		void removeActiveScenes(std::deque<std::string> ids) { 
-            for (auto &id : ids) 
-                activeScenes.erase(std::remove(activeScenes.begin(), activeScenes.end(), id), activeScenes.end()); 
-        }
-		void clearActiveScenes(std::string id) { activeScenes.erase(std::remove(activeScenes.begin(), activeScenes.end(), id), activeScenes.end()); }
-
-		void renderActiveScenes() {
+		//
+		// Other functions
+		//
+		void rayscenemanager::renderActiveScenes() {
 			std::deque<std::string> temp(activeScenes);
-            for (auto &i : temp) {
-                if (scenes.find(i) != scenes.end()) 
-                    scenes[i].call(
-                        *this,
-                        activeScenes.back() == i
-                    );
-            }
+			for (auto &i : temp) 
+				if (scenes.find(i) != scenes.end())
+					scenes[i].call(*this, activeScenes.back() == i);
 		}
 
-	private:
-		std::unordered_map<std::string, rayscene> scenes = {};
-		std::deque<std::string> activeScenes = {};
-	
+		//
+		// Private utility functions
+		//
 		template<typename T>
-		void clear_deque( std::deque<T> &q ) {
+		void rayscenemanager::clear_deque(std::deque<T> &q) {
 			std::deque<T> empty;
-			std::swap( q, empty );
+			std::swap(q, empty);
 		}
-	
 		template<typename K, typename V>
-		void clear_unordered_map( std::unordered_map<K, V> &q ) {
+		void rayscenemanager::clear_unordered_map(std::unordered_map<K, V> &q) {
 			std::unordered_map<K, V> empty;
-			std::swap( q, empty );
+			std::swap(q, empty);
 		}
-	};
-
 }
